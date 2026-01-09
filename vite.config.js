a6c2f6a8a318868b7a68f1d4ab800216dcc27c32
atarax <atarax@example.com>
2026-01-09 15:39:48 +0300

    Base done

--- vite.config.js
+++ vite.config.js
@@ -9,20 +9,55 @@ export default defineConfig({
 	//настройки сервера
 	server: {
 		//настройки сервера
-		open: true, //открывать браузер автоматически
-		port: 3000, //порт на котором будет работать сервер
-		host: true, //чтобы сервер работал на всех устройствах
+		open: true,
+		port: 3000,
+		host: true,
 	},
 
 	//настройки путей к файлам
 	resolve: {
 		alias: {
 			//  алиасы для путей к файлам
-			'@': path.resolve(__dirname, '.') /* корневой каталог */,
-			'@scss': path.resolve(__dirname, 'scss') /* scss каталог */,
-			'@js': path.resolve(__dirname, 'js') /* js каталог */,
-			'@img': path.resolve(__dirname, 'assets', 'img') /* img каталог */,
-			'@fonts': path.resolve(__dirname, 'assets', 'fonts') /* fonts каталог */,
+			'@': path.resolve(__dirname, '.'),
+			'@scss': path.resolve(__dirname, 'scss'),
+			'@js': path.resolve(__dirname, 'js'),
+			'@img': path.resolve(__dirname, 'assets', 'img'),
+			'@fonts': path.resolve(__dirname, 'assets', 'fonts'),
 		},
 	},
+
+	// настройки сборки
+	build: {
+		outDir: 'dist',
+		emptyOutDir: true,
+		assetsDir: 'assets',
+		sourcemap: 'hidden',
+		minify: 'esbuild',
+		cssMinify: 'esbuild',
+		rollupOptions: {
+			output: {
+				assetFileNames: (assetInfo) => {
+					let extType = assetInfo.name.split('.').at(-1);
+					if (/png|jpe?g|svg|gif|webp|avif/i.test(extType)) {
+						return 'assets/img/[name]-[hash][extname]';
+					}
+					if (/woff2?|ttf|otf|eot/i.test(extType)) {
+						return 'assets/fonts/[name]-[hash][extname]';
+					}
+					return 'assets/[name]-[hash][extname]';
+				},
+				chunkFileNames: 'assets/js/[name]-[hash].js',
+				entryFileNames: 'assets/js/[name]-[hash].js',
+				manualChunks: (id) => {
+					if (id.includes('node_modules')) {
+						return 'vendor';
+					}
+				},
+			},
+		},
+	},
+	css: {
+		devSourcemap: true,
+	},
+	base: './',
 });
